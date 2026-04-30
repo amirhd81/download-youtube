8a44be2eb9b6fab9e2aa8f1c32d6111085945f9b
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-04-30 22:33:34 +0330

    fds

--- sj.js
+++ sj.js
@@ -8,20 +8,23 @@ const browser = await chromium.launch({
     executablePath: chromiumPath,
     headless: true
    });
+
+    let clicked = false;
   const logFile = fs.createWriteStream('network_log.txt', { flags: 'a' });
   
   const page = await browser.newPage();
 
+    page.on('request', req => {
+  if (clicked && req.method() === 'POST') {
+      logFile.write(`POST ${req.url()}\nBODY: ${req.postData()}\n\n`);
+  }
+});
 
-  page.on('request', request => {
-    logFile.write(`\n>>> REQUEST: ${request.method()} ${request.url()}\n`);
-    if (request.method() === 'POST') {
-      logFile.write(`BODY: ${request.postData() || 'none'}\n`);
-    }
-  });
 
   page.on('response', async response => {
+      if (clicked) {
     logFile.write(`<<< RESPONSE: ${response.status()} ${response.url()}\n`);
+      }
   });
 
   await page.goto('https://streamable.com/ri37ps');
@@ -29,6 +32,7 @@ const browser = await chromium.launch({
   await page.fill("#password", "gvc277");
 
   await page.click('button[type="submit"]');
+    clicked = true
 
   logFile.end();
   
